af76eaf821a95b1f3af3a0a06f619669d0eb3e74
Marco Antonio Ortiz Ramirez <131299835+Marko24K@users.noreply.github.com>
2024-12-30 12:52:52 -0300

    30-12
    h

--- Datos/models.py
+++ Datos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Sensor(models.Model):
+    nombre_sensor = models.CharField(max_length=50)
+    tipo_sensor = models.CharField(max_length=50)
+    ubicacion = models.TextField()
+    descripcion = models.TextField()
+    fecha_registro = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.nombre_sensor
+    
+class Tipo_dato(models.Model):
+    nombre_tipo_dato = models.CharField(max_length=50)
+    unidad = models.CharField(max_length=20)
+    def __str__(self):
+        return self.nombre_tipo_dato
+
+class Datos_sensores(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    tipo_dato = models.ForeignKey(Tipo_dato, on_delete=models.CASCADE,null = False)
+    valor = models.FloatField()
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sensor.nombre_sensor} - {self.valor}"
--- Sistema/settings.py
+++ Sistema/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'Sistema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Humedal',
+        'USER':'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
+    
 }
 
 
